8885828b9266f8b84339854f1b1acd25310ae0b0
milica <milicagnjatovic18@gmail.com>
2021-08-23 23:11:31 +0200

    added projects

--- docs/public/js/index.js
+++ docs/public/js/index.js
@@ -19,18 +19,31 @@ function loadJSON(){
             main_div.className = "project"
             main_div.onclick = function() {showAll(this);}
 
-            var div_title = document.createElement("div")
+            var div_title = document.createElement("h1")
             div_title.className = "project_title"
             var title = document.createTextNode(data.project[i].title)
             div_title.appendChild(title)
 
-            var div_text = document.createElement("div")
-            div_text.className = "project_content"
+            var div_content = document.createElement("div")
+            div_content.className = "project_content"
+
+            var text_tag = document.createElement("p")
             var text = document.createTextNode(data.project[i].text)
-            div_text.appendChild(text)
+            text_tag.appendChild(text)
+
+            var link_tag = document.createElement("a")
+            link_tag.href = data.project[i].link
+            link_tag.text = "Više..."
+
+            var img_tag = document.createElement("img")
+            img_tag.src = data.project[i].image
+        
+            div_content.appendChild(text_tag)
+            div_content.appendChild(link_tag)
+            div_content.appendChild(img_tag)
 
             main_div.appendChild(div_title)
-            main_div.appendChild(div_text)
+            main_div.appendChild(div_content)
 
             el.appendChild(main_div)
         }
