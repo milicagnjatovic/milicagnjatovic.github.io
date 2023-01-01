b24bed567953c9d37154b5ca74d75a4963d73bb2
milica <milicagnjatovic18@gmail.com>
2023-01-01 22:08:11 +0100

    Sredjeni rokovi, porpavljeno obavestenje.

--- docs/public/js/gen_cas.js
+++ docs/public/js/gen_cas.js
@@ -82,14 +82,50 @@ function dohvatiRokove(){
     fetch("../../resources/rbp/rokovi/files.json") // vr=1.0 da bi se video najnoviji fajl
     .then(response => response.json())
     .then(data => {
-        for(let rok of data.files){
-            // console.log(rok)
-            let item = document.createElement('a');
-            item.href = "./resources/rbp/rokovi/" + rok;
-            item.target='_blank';
-            item.text = rok 
-            item.style.display = 'block'
-            root.append(item);
+        let tabela = document.createElement('table');
+        tabela.className = "tabela_rokova";
+        root.append(tabela)
+        for(let godina of data.godine){
+            let n = data.files[godina].length;
+            let tr = document.createElement('tr');
+            tabela.append(tr)
+            for(let i=0; i<n; i++){
+                let rok = data.files[godina][i];
+
+                let cell = document.createElement('td');
+                tr.append(cell);
+                
+                let cb = document.createElement('input');
+                cb.type = 'checkbox'
+                if (!localStorage.hasOwnProperty(rok)){
+                    localStorage[rok] = "0";
+                }
+                cb.checked = localStorage[rok]=="1";
+                cb.onclick = () => {
+                    console.log(rok)
+                    localStorage[rok] = localStorage[rok]=="0" ? "1" : "0";
+                }
+                cell.append(cb);
+                
+                let item = document.createElement('a');
+                item.href = "./resources/rbp/rokovi/" + rok;
+                item.target='_blank';
+                item.text = rok 
+                item.style.display = 'block'
+                cell.append(item);
+
+                if(i+1<n && data.files[godina][i+1].startsWith('res')){
+                    console.log("resenje");
+                    let item = document.createElement('a');
+                    item.href = "./resources/rbp/rokovi/" + data.files[godina][i+1];
+                    item.target='_blank';
+                    item.text = "rešenje" 
+                    item.style.display = 'block'
+                    cell.append(item);
+                    cell.style.border = "solid darkblue 5px"
+                    i++;
+                }
+            }
         }
         let item = document.createElement('a');
         item.href = "http://poincare.matf.bg.ac.rs/~nenad/rbp/ispit/rbp.rokovi.html";
